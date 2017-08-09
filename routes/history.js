507dbc60119241f4a2d0e4281f61f92b131d1a89
Connor J Smith <connorjsmith101@gmail.com>
2017-08-09 16:10:34 -0400

    Added pdf history book

--- routes/history.js
+++ routes/history.js
@@ -7,4 +7,8 @@ router.get('/', function(req, res, next) {
   res.render('history', {title: "Cannon History"});
 });
 
+router.get('/history_book', function(req, res, next) {
+  res.render('history_book', { layout: null });
+});
+
 module.exports = router;
