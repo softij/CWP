54b844af9c7e006cb351a78b02cf1f2e26e818d8
Connor J Smith <connorjsmith101@gmail.com>
2017-08-16 15:51:30 -0400

    Styling and updated scripts

--- start_release.sh
+++ start_release.sh
@@ -1,1 +1,1 @@
-npm start
+DATABASE_URL=postgres://yrnvimhinrdxit:784bf0573180d6302715d50e6e12e09c75520d52f33711a98db76714d7b6c7ff@ec2-54-221-221-153.compute-1.amazonaws.com:5432/dffacbaugdh2jo npm start
