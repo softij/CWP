a48e1c32a1915293464952faa20d943e1b38018c
Connor Smith <connorjsmith101@gmail.com>
2017-08-19 18:38:06 -0400

    Added FAQ page (#5)

---
+++ routes/faq.js
@@ -0,0 +1,10 @@
+const Router = require('express-promise-router');
+
+const router = new Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('faq', {title:"Cannon Home"});
+});
+
+module.exports = router;
