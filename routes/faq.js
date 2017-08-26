3510864d6ea48c46bfceec12581f4d7da8c89a24
Connor J Smith <connorjsmith101@gmail.com>
2017-08-25 21:34:00 -0400

    Fixed page titles, added more pictures

--- routes/faq.js
+++ routes/faq.js
@@ -4,7 +4,7 @@ const router = new Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('faq', {title:"Cannon Home"});
+  res.render('faq', {title:"Frequently Asked Questions"});
 });
 
 module.exports = router;
--- routes/publications.js
+++ routes/publications.js
@@ -4,7 +4,7 @@ const router = new Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('publications', {title:"Cannon Home"});
+  res.render('publications', {title:"Publications and Media"});
 });
 
 module.exports = router;
