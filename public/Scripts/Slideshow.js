8bc7a95617c707aed5a0fd0027365b3c900e1f2c
michaelvu97 <michael.vu.97@outlook.com>
2017-08-31 23:44:37 -0400

    added new slideshow images

--- public/Scripts/Slideshow.js
+++ public/Scripts/Slideshow.js
@@ -2,11 +2,9 @@
  * Create a slideshow
  * TODO make this non-static/configurable
  */
-const IMAGE_URLS = [
-        'https://photos.smugmug.com/1T61T7/Frosh-Week-1T6/Day-1/Matriculation/i-JDSxNRb/0/62924396/X2/IMG_0803-X2.jpg',
-        'https://photos.smugmug.com/1T61T7/Other-Events/Last-Suds/i-b2nsKdv/0/ed15dbe2/X2/IMG_7023.edit-X2.jpg',
-        'https://photos.smugmug.com/1T61T7/Godiva-Week/Godivas-Resurection-/i-BfNcZms/0/60022f5d/X2/IMG_0013-X2.jpg'
-];
+const IMAGE_PATH = "/Images/slideshow/{0}.jpg";
+
+const NUM_IMAGES = 7;
 
 (function() {
 
@@ -15,9 +13,9 @@ const IMAGE_URLS = [
     if (typeof slideshowDiv === "undefined")
         return;
 
-    for (var i = 0; i < IMAGE_URLS.length; i++) {
+    for (var i = 0; i < NUM_IMAGES; i++) {
         
-        var url = IMAGE_URLS[i];
+        var url = IMAGE_PATH.replace('{0}', i);
 
         $(slideshowDiv).append($(CreateSlide(i, url)));
     }
